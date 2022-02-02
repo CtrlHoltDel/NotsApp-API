803c4ba444ff3ddb3d5ab9004109fd497ea5ac16
CtrlHoltDel <ctrlholtdel@gmail.com>
2022-02-02 23:28:04 +0000

    Updated port for heroku

--- listen.js
+++ listen.js
@@ -1,6 +1,8 @@
 const app = require("./app");
 
-const PORT = 3000;
+const { PORT = 3000 } = process.env;
+
+console.log(PORT, "<<<");
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
